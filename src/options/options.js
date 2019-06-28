78161ec14afdd21b16b87b9c678f96735a4f227a
takrishna <takrishna@gmail.com>
2019-06-28 15:58:44 +0200

    Update options.js

--- src/options/options.js
+++ src/options/options.js
@@ -2,7 +2,9 @@
 
 //Read config from local storage
 //This is setup in setup.js - by listening onInstalled listener
-var eee = "";
+var eee = "",
+    previousValue ="";
+
 chrome.storage.local.get("specs", function (config) {
   require([
     'vs/basic-languages/monaco.contribution',
@@ -18,9 +20,27 @@ chrome.storage.local.get("specs", function (config) {
 
   editorDidMount(eee, monaco);
   function editorDidMount(editor, monaco) {
-    setTimeout(function () {
-      eee.trigger('anyString', 'editor.action.formatDocument');
-    }, 300);
+    setTimeout(function () {triggerFormat()},300);
+      previousValue=eee.getValue();
   }
   return;
 });
+
+function triggerFormat(){
+    eee.trigger('anyString', 'editor.action.formatDocument');
+}
+
+document.getElementById("saveBtn").addEventListener("click", save);
+function save(){
+  chrome.storage.local.set(JSON.parse(eee.getValue()), function() {
+    previousValue=eee.getValue();
+    document.getElementById("saveStatus").setAttribute("style","visibility:visible;background: #d8f398;"); 
+    setTimeout(function(){document.getElementById("saveStatus").setAttribute("style","visibility:hidden"); },3000);
+  })
+}
+
+document.getElementById("resetBtn").addEventListener("click", reset);
+function reset(){
+    eee.setValue(previousValue);
+    triggerFormat();
+}

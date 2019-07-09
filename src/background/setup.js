014ee346c1d56cce7d5e2b68cad3f04aa0d350f0
abinayaramesh <takrishna@gmail.com>
2019-07-09 21:49:00 +0200

    format

--- src/background/setup.js
+++ src/background/setup.js
@@ -1,15 +1,14 @@
 // Read configuration from Default config file (config/config.json) 
 // and store in local storage
 'use strict';
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
   fetch("/config/config.json")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(configJson) {
-    chrome.storage.local.set(configJson, function() {
+    .then(function (response) {
+      return response.json();
     })
-    chrome.storage.local.set({'autoOpen':true}, function(data) {
+    .then(function (configJson) {
+      configJson.autoOpen = true;
+      chrome.storage.local.set(configJson, function () {
+      });
     });
-  });
 });

d07fb66bced3c51e6acaa056960944340711b6be
takrishna <takrishna@gmail.com>
2019-07-12 14:44:34 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
 const gulp = require('gulp');
 const terser = require('gulp-terser');
+const clean = require('gulp-clean');
 
-function es(){
-  return gulp.src('./src/*/*.js')
+//es build
+function esbuild(){
+  gulp.src('./src/**/*.js')
     .pipe(terser())
-    .pipe(gulp.dest('./build'))
+    .pipe(gulp.dest('./build'));
+  gulp.src(['./options/vs/*.css'])
+    .pipe(gulp.dest('./build/options/vs/'));
+  gulp.src(['./images/*.*'])
+    .pipe(gulp.dest('./build/images'));
+  gulp.src(['./config/*.*'])
+    .pipe(gulp.dest('./build/config'));
+  return gulp.src(['./src/**/*.html','manifest.json'])
+    .pipe(gulp.dest('./build'));
 }
 
-gulp.task('default', es);
+//clean 
+async function  prepare(){
+  gulp.src('./build', {read: false, allowEmpty: true})
+    .pipe(clean());
+}
+gulp.task('clean', prepare);
+gulp.task('esbuild', esbuild);
+
+gulp.task('default', gulp.series('clean','esbuild'));
